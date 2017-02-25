ab00fd9b1eaba1ef7d061d41e9ed3dc2bb549364
Josh Matos <josuematos87@gmail.com>
2017-02-24 22:38:02 -0600

    first steps

---
+++ README.md
@@ -0,0 +1,32 @@
+Movie Seach
+---
+ 
+Welcome.
+ 
+ 
+ 
+Usage
+---
+ 
+Start the development server with this command:
+ 
+```
+npm start
+```
+ 
+ 
+ 
+Setup
+---
+ 
+```
+npm install
+```
+ 
+ 
+ 
+Compile
+---
+ 
+```
+npm run compile
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+console.log($('#id'.style))
+ 
+/**
+ * A counter button: tap the button to increase the count.
+ */
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  componentDidMount(){
+
+  }
+ 
+  render() {
+    return (
+    	<div>
+       Round 2 
+      </div>
+      
+    );
+  }
+}
+export default App;
